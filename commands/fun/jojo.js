bed5876e3e8e784225e4719280c80e859b11cc97
Steven Botez <stbotez@gmail.com>
2023-06-05 23:01:55 -0400

    Added many fun commands

---
+++ commands/fun/jojo.js
@@ -0,0 +1,10 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('jojo')
+		.setDescription('Replies with buddy'),
+	async execute(interaction) {
+		await interaction.reply('buddy');
+	},
+};
