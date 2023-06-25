788573c0f7157723ace03d9849e0e7da56a4ec33
Steven Botez <stbotez@gmail.com>
2023-06-25 00:02:40 -0400

    Add check for empty HTTP body

--- commands/fun/imagesearch.js
+++ commands/fun/imagesearch.js
@@ -1,5 +1,10 @@
 const path = require("node:path");
-const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+const fs = require("fs");
+const {
+  SlashCommandBuilder,
+  EmbedBuilder,
+  AttachmentBuilder,
+} = require("discord.js");
 const { request } = require("undici");
 const { googleAPIKey, searchEngineId } = require(path.join(
   process.cwd(),
@@ -24,11 +29,29 @@ module.exports = {
     ),
   async execute(interaction) {
     const query = interaction.options.getString("query");
-    logger.info(`User query provided: ${query}`);
+    logger.info(`User query: ${query}`);
     const searchType = "image";
-    const imgResult = await request(`
+    const responseData = await request(`
       https://www.googleapis.com/customsearch/v1?key=${googleAPIKey}&cx=${searchEngineId}&q=${query}&searchType=${searchType}`);
-    const { items } = await imgResult.body.json();
+    logger.info("Header information: ");
+    logger.info(responseData.headers);
+    const { items } = await responseData.body.json();
+    if (items === undefined) {
+      const budInvert = new AttachmentBuilder(
+        path.join(process.cwd(), "assets", "img", "buddyInvert.png")
+      );
+      logger.info("Returned result has no body");
+      const embed = new EmbedBuilder()
+        .setColor("Red")
+        .setTitle("No results found")
+        .setThumbnail("attachment://buddyInvert.png")
+        .addFields({ name: "Query", value: query, inline: true });
+
+      return interaction.reply({ embeds: [embed], files: [budInvert] });
+    }
+    logger.info(`HTTP response code is ${responseData.statusCode}`);
+    logger.info("Body: ");
+    logger.info(items);
     const randResult = items[getRandomIntInclusive(0, items.length - 1)];
     const embed = new EmbedBuilder()
       .setColor("Blue")
