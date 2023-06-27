134ff776a5a3804bf2ec31daf2151bacebfa8ec5
Steven Botez <stbotez@gmail.com>
2023-06-26 22:09:46 -0400

    Begin migration of config.json to .env

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const fs = require("node:fs");
 const path = require("node:path");
 const { Client, Collection, GatewayIntentBits } = require("discord.js");
-const { token } = require("./config.json");
+const dotenv = require("dotenv").config();
 const logger = require(path.join(__dirname, "util", "logger.js"));
 
 // Create a new client instance
