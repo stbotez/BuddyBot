c2085c5ca1b00471179316753ebdafc500d276e6
Steven Botez <stbotez@gmail.com>
2023-07-01 18:01:56 -0400

    Calculate startInd based on user-provided index

--- commands/fun/imagesearch.js
+++ commands/fun/imagesearch.js
@@ -35,11 +35,19 @@ module.exports = {
     ),
   async execute(interaction) {
     const query = interaction.options.getString("query");
-    logger.info(`User query: ${query}`);
+    const index = interaction.options.getInteger("index") ?? -1;
     const searchType = "image";
-    const startInd = getRandomIntInclusive(0, 80);
+    const resultsPerPage = 10;
+    let startInd;
+    // no user-provided index, return a random image by default
+    if (index == -1) {
+      startInd = getRandomIntInclusive(0, 90);
+    } else {
+      startInd = getPageofIndex(index) * resultsPerPage;
+    }
     const res = await request(`
       https://www.googleapis.com/customsearch/v1?key=${googleAPIKey}&cx=${searchEngineId}&q=${query}&searchType=${searchType}&start=${startInd}`);
+    logger.info(`User query: ${query}`);
     logger.info("Header: ");
     logger.info(res.headers);
     const body = await res.body.json();
@@ -56,11 +64,15 @@ module.exports = {
       await interaction.reply({ embeds: [embed], files: [budInvert] });
     }
 
-    const randResult =
-      body.items[getRandomIntInclusive(0, body.items.length - 1)];
-    logger.info(
-      `Randomly chosen result: ${JSON.stringify(randResult, null, 4)}`
-    );
+    let image = null;
+    if (index == -1) {
+      image = body.items[getRandomIntInclusive(0, body.items.length - 1)];
+      logger.info(
+        `Randomly chosen result: ${JSON.stringify(randResult, null, 4)}`
+      );
+    } else {
+      image = body.items;
+    }
     const embed = new EmbedBuilder()
       .setColor("Blue")
       .setTitle(randResult.title)
@@ -70,3 +82,14 @@ module.exports = {
     await interaction.reply({ embeds: [embed] });
   },
 };
+
+function getPageofIndex(ind) {
+  let lowerBound = 0;
+
+  if (ind > 9) {
+    lowerBound = ind - (ind % 10);
+  }
+  return lowerBound / 10;
+}
+
+// TODO: Probably move this function to my helper class.
