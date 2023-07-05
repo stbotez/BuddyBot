f9f05c7da76e60c2fd8f3f318492e935c53a8e87
Steven Botez <stbotez@gmail.com>
2023-07-05 16:45:38 -0400

    Adjust export syntax to be consistent with rest of project

--- util/helper.js
+++ util/helper.js
@@ -1,14 +1,16 @@
-exports.getRandomIntInclusive = (min, max) => {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
+module.exports = {
+  getRandomIntInclusive: (min, max) => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
+  },
+
+  getPageOfImageIndex: (ind) => {
+    let page = 0;
+
+    if (ind > 9) {
+      page = ind - (ind % 10);
+    }
+    return page / 10;
+  },
 };
-
-async function getPageOfImageIndex(ind) {
-  let page = 0;
-
-  if (ind > 9) {
-    page = ind - (ind % 10);
-  }
-  return page / 10;
-}
